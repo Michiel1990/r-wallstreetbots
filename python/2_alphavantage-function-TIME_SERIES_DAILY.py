a590e969127cf15386e0aee6c22f8675d0907896
Michiel-at-Biztory <smulders.michiel@icloud.com>
2025-10-01 16:15:23 +0200

    commit

--- python/2_alphavantage-function-TIME_SERIES_DAILY.py
+++ python/2_alphavantage-function-TIME_SERIES_DAILY.py
@@ -28,8 +28,10 @@ engine = create_engine(connection_string)
 
 # Fetch the next 12 companies to fetch data from and store in a list
 query_tickers = """
-    select "symbol" as ticker from rawalphavantage.listing_status
-    where "symbol" in ('MSFT','GOOGL','AMZN');
+    select str_company_ticker as ticker from devsilver.dim_companies
+    where bl_data_missing_at_dt_min_one = true
+    order by dt_ipo asc
+    limit 12;
 """
 df_tickers = pd.read_sql(query_tickers, engine)
 tickers = df_tickers['ticker'].tolist()
