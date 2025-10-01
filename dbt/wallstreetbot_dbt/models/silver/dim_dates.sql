626c356969432c5c9c8daafff6ae85853bc3f2c6
Michiel-at-Biztory <smulders.michiel@icloud.com>
2025-10-01 18:35:24 +0200

    Update dim_dates.sql

--- dbt/wallstreetbot_dbt/models/silver/dim_dates.sql
+++ dbt/wallstreetbot_dbt/models/silver/dim_dates.sql
@@ -1,6 +1,15 @@
+with cte_companies_with_data as (
+  select 
+    str_company_ticker
+    ,min(dt_price) as earliest_dt_price
+  from {{ ref('fact_stock_prices') }}
+  group by str_company_ticker
+)
+
 select
   c.str_company_ticker
   ,d.date_day
   ,d.quarter_end_date
-from {{ ref('dim_companies') }} c
-  ,{{ ref('dbt_get_dates') }} d
+from cte_companies_with_data c
+left join {{ ref('dbt_get_dates') }} d
+  on c.earliest_dt_price <= d.date_day
